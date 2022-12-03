62525fba72efc716f408813348a3f0101147be20
Yakov Popov <suburbanms@gmail.com>
2022-12-03 23:19:55 +0700

    last

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 let numberOne = 5, numberTwo = 10, sum, string;
 sum = numberOne + numberTwo;
-document.write("Выражение: \"" + numberOne + " + " + numberTwo + "\"." + 
+document.write("Выражение: \"" + numberOne + " + " + numberTwo + "\"." +
 "<br \/>Ответ: " + sum);
-
+ 
 document.write(
 "<br \/> Функция для преобразования строки в верхний регистр: \"stringObj.toUpperCase()\"" +
 "<br \/>Функция для преобразования строки в нижний регистр: \"stringObj.toLowerCase()\"");
-
+ 
 string = "Hello From Kemorovo";
-document.write("<br \/> Пример работы:<br \/>" + 
+document.write("<br \/> Пример работы:<br \/>" +
 "Исходная строка -"+ string + "<br \/>"
  + string.toLowerCase() + "- нижний регистр<br \/>" +
   string.toUpperCase() + " - верхний регистр");
-
+ 
 document.write("<br \/>" + "5 - 10 = " + (numberOne - numberTwo));
 document.write("<br \/>" + "5 * 10 = " + (numberOne * numberTwo));
 document.write("<br \/>" + "5 / 10 = " + (numberOne / numberTwo));
@@ -22,7 +22,19 @@ document.write("<br \/>" + "++5 = " + (++numberOne));
 numberOne = 5;
 document.write("<br \/>" + "--5 = " + (--numberOne));
 numberOne = 5;
-
-document.getElementById("button").onclick = function(){
-  alert("Сибиряков Матвей, ПИ-202");
+ 
+function BtnClick1(){
+    alert("Сибиряков Матвей, ПИ-202");
+}
+ 
+var str = "";
+ 
+function BtnClick2(){
+    str = str + document.getElementById("input").value;
+    result82.innerHTML = str;
+}
+ 
+function BtnClick3(){
+    str = "";
+    result82.innerHTML = str;
 }
